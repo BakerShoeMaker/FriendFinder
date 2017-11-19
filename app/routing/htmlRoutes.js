a4d27711442fd8737f32ef7116eb9d9dc159a80b
BakerShoeMaker <info@quackenworth.com>
2017-11-19 10:20:36 -0800

    fixed path issue.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
 
     app.get("/", (req, res)=>{
         //res.send("You're on the Index page.");
-        res.sendFile(path.join(__dirname, "/app/public/home.html"));
+        //res.sendFile(path.join(__dirname, "/app/public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     app.get("/survey", (req, res)=> {
         res.send("You're on the survey page.");
